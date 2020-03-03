0be9f81f62771d9128abea0d29e9c6ccbc25ea17
Tomas O'Malley <moss.omalley@gmail.com>
2020-03-03 20:31:16 +0000

    Added Navigation

--- App.js
+++ App.js
@@ -1,160 +1,67 @@
-import React from 'react';
-import {  Image } from 'react-native';
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { SplashScreen } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import useLinking from './navigation/useLinking';
 
-import {
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-} from 'react-native';
-import Constants from 'expo-constants';
-//      <Image source={require('./assets/icon.png')} />
+const Stack = createStackNavigator();
 
-function Separator() {
-  return <View style={styles.separator} />;
-}
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const [initialNavigationState, setInitialNavigationState] = React.useState();
+  const containerRef = React.useRef();
+  const { getInitialState } = useLinking(containerRef);
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.title}>
-         Welcome
-        </Text>
-        <Button
-          title="Nightlife"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
-         <Image
-          style={{width: 50, height: 50}}
-          source={require('./assets/beer-outline.svg')}
-        />
+  // Load any resources or data that we need prior to rendering the app
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load our initial navigation state
+        setInitialNavigationState(await getInitialState());
+
+        // Load fonts
+        await Font.loadAsync({
+          ...Ionicons.font,
+          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return null;
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the backgroud color of the button.
-        </Text>
-        <Button
-          title="Student Calender"
-          color="grey"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-
-        <Image
-          style={{width: 50, height: 50 }}
-          source={require('./assets/calendar-outline.svg')}
-        />
-      </View>
-      <Separator />
-
-       <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the backgroud color of the button.
-        </Text>
-        <Button
-          title="Student Accomodation"
-          color="red"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-
-        <Image
-          style={{width: 50, height: 50 }}
-          source={require('./assets/home-outline.svg')}
-        />
-      </View>
-      <Separator />
-
-
-
-
-
-       <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the backgroud color of the button.
-        </Text>
-        <Button
-          title="Student Grinds Exchange"
-          color="green"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-
-        <Image
-          style={{width: 50, height: 50 }}
-          source={require('./assets/phone-portrait-outline.svg')}
-        />
-      </View>
-      <Separator />
-
-
-
-    <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the backgroud color of the button.
-        </Text>
-        <Button
-          title="Online Student ID"
-          color="#f194ff"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-
-        <Image
-          style={{width: 50, height: 50 }}
-          source={require('./assets/card-outline.svg')}
-        />
-      </View>
-      <Separator />
-<View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the backgroud color of the button.
-        </Text>
-        <Button
-          title="Student Forums"
-          color="orange"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-
-        <Image
-          style={{width: 50, height: 50 }}
-          source={require('./assets/book-outline.svg')}
-        />
-      </View>
-      <Separator />
-
-    </SafeAreaView>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+    backgroundColor: '#fff',
   },
 });
