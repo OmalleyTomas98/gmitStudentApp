7075871650cf4251c6e9c4e29cec1745e7ed21e8
BenasPelakauskas <g00356158@gmit.ie>
2020-03-11 14:00:25 +0000

    Rent component error fixed

--- screens/Rent.js
+++ screens/Rent.js
@@ -5,8 +5,22 @@ import {StyleSheet, View, ScrollView, Text} from 'react-native';
 export default class Rent extends Component {
     render() {
         return (
-            <h1 align="center">Search for Student Accomodation</h1>
+          <Text style={styles.headerRent}>Rent Student Accomodation:</Text>
         );
       }
     }
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+      },
+      headerRent: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 26,
+        lineHeight: 19,
+        textAlign: 'center',
+    },
+  });
 
